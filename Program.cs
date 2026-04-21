bc3829deb8da15bafbd2b60d376c8e66ab125e03
Aicha MT <1770667@cgodin.qc.ca>
2026-04-20 21:40:47 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Skolaris_Horaires.Components;
+using Skolaris.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Register application services (in-memory implementations)
+builder.Services.AddSingleton<ICoursService, CoursService>();
+builder.Services.AddSingleton<IEmploiDuTempsService, EmploiDuTempsService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
